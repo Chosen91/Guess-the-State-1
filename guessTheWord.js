427989dc71d92413c3d7e98186d7e8c1f11851ef
Markteisha Gill <markteishagill@Markteishas-MacBook-Pro.local>
2022-05-03 11:59:00 -0400

    final project

--- guessTheWord.js
+++ guessTheWord.js
@@ -1,12 +1,11 @@
 const log = (arg) => console.log(arg)
 
-// Step 1: Since the premise of the came is to be able to guess the name of a state. Create a list of states and put into an array. 
+// Step 1: Sice the premise of the came is to be able to guess the name of a state. Create a list of states and put into an array. 
 // I tried to find a API to use, but was having diffuclities locating one. 
 
 var states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','DistrictofColumbia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','NewHampshire','NewJersey','NewMexico','NewYork','NorthCarolina','NorthDakota','Ohio','Oklahoma','Oregon','Pennsylvania','PuertoRico','RhodeIsland','SouthCarolina','SouthDakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','WestVirginia','Wisconsin','Wyoming']
 
-// Since the code is relying only none numeric or special chracters. I removed the space from certain two word states. 
-
+// Since my code is relying only none numeric or special chracters. I removed the space from certain two state states. 
 
 
 var game = {
@@ -21,48 +20,6 @@ var game = {
     gamePlay: true
 }
 
-/* Step 2
-Create a funtion that would start the game. What should the function include?
-(1) The game should start off with a white background. 
-(2) The status bar should be green. So remove any and all previous colors and add bg-success.
-(3) All following arrays should be emptied stateLetters, correctGuess, placeHolder, and wrongGusses. 
-*/
-function newGamePrep() {
-
-// Game needed to start with the bg-success... Since the game may end with bg danger or warning. Remove both and add bg success
-    
-    document.getElementById("status").style.backgroundColor = "#e9ecef";
-    document.getElementById("progress").classList.remove("bg-danger")
-    document.getElementById("progress").classList.remove("bg-warning")
-    document.getElementById("progress").classList.add("bg-success")
-    document.getElementById("progress").setAttribute("style", "width: 100%;");
-    game.state = states[Math.floor(Math.random() * states.length)]
-    game.stateLetters = [];
-    game.correctGuess = [];
-    game.placeHolder = [];
-    game.wrongGuess = [];
-    
-    document.getElementById("message").innerHTML = ""
-    document.getElementById("wrong").innerHTML = ""
-    game.gamePlay = true;
-
-
-    //splits the state into letters into the stateLetters array
-    for (i = 0; i < game.state.length; i++) {
-        game.stateLetters.push(game.state.charAt(i).toLowerCase())
-    }
-   // The code below takes the individuals letters in the state and mask them with _. 
-   // it also provides a slight hint due to the number of spaces. California and Iowa are different in letter count.
-    for (i = 0; i < game.stateLetters.length; i++) {
-        game.placeHolder.push("_")
-    }
-    var place = ""
-    for (i = 0; i < game.placeHolder.length; i++) {
-        place += "_ "
-    }
-    log(game.stateLetters)
-    document.getElementById("state").innerHTML = place;
-}
 
 
 // 
@@ -76,9 +33,7 @@ function newGame() {
          https://css-tricks.com/snippets/javascript/javascript-keycodes/*/
 
         if (event.which >= 65 && event.which <= 90) {
-            document.getElementById("coolkid").classList.add("rollIn")
             var keyPressed = (event.key).toLocaleLowerCase()
-
         } else {
             //Instead of using an Alert, I am using a Modal to alert the user that the input they just entered was not a letter
             //alert("Only letters A-Z are allowed")
@@ -152,7 +107,7 @@ function newGame() {
                         document.getElementById("message").innerHTML = "Letter already tried";
                     }
                 }
-                else //no more tries, game is OVER
+                else //no more tries, you loose
                 {
                     while (game.gamePlay) {
                         gameOver();
@@ -177,8 +132,6 @@ function newGame() {
     }
     
 }
-
-// Function if player loses game
 function gameOver() {
     var displayLetters = ""
     log("game over")
@@ -229,7 +182,45 @@ function winsGame() {
     game.gamePlay = false;
 
 }
+/* 
+Create a funtion that would start the game. What should the function include?
+(1) The game should start off with a white background. 
+(2) The status bar should be green. So remove any and all previous colors and add bg-success.
+(3) All following arrays should be emptied stateLetters, correctGuess, placeHolder, and wrongGusses. 
+*/
+function newGamePrep() {
+    document.getElementById("status").style.backgroundColor = "#e9ecef";
+    document.getElementById("progress").classList.remove("bg-danger")
+    document.getElementById("progress").classList.remove("bg-warning")
+    document.getElementById("progress").classList.add("bg-success")
+    document.getElementById("progress").setAttribute("style", "width: 100%;");
+    game.state = states[Math.floor(Math.random() * states.length)]
+    game.stateLetters = [];
+    game.correctGuess = [];
+    game.placeHolder = [];
+    game.wrongGuess = [];
+    
+    document.getElementById("message").innerHTML = ""
+    document.getElementById("wrong").innerHTML = ""
+    game.gamePlay = true;
 
+
+    //splits the state into letters into the stateLetters array
+    for (i = 0; i < game.state.length; i++) {
+        game.stateLetters.push(game.state.charAt(i).toLowerCase())
+    }
+    //Since the majority of states have different number of chracters. Create a ** HINT ** that wioll help them identify 
+    // the number of letters . Best way t6o accomplish this is by using spaces. 
+    for (i = 0; i < game.stateLetters.length; i++) {
+        game.placeHolder.push("_")
+    }
+    var place = ""
+    for (i = 0; i < game.placeHolder.length; i++) {
+        place += "_ "
+    }
+    log(game.stateLetters)
+    document.getElementById("state").innerHTML = place;
+}
 
 
 
